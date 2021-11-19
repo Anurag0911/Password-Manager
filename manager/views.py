f18a9d70e96f831163b793d659ef24a155476def
Anurag0911 <anurag.asy81@gmail.com>
2021-11-19 07:57:10 +0530

    Update: GUI

--- manager/views.py
+++ manager/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+
+import json
+
 from .models import Password
 from . import db
-import json
+
+
 
 views = Blueprint('views', __name__)
 
